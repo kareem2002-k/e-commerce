894744a73f5a7b7f9c0deab3ddc25665eec4f14c
Kareem Ahmed <kareemsamir166@gmail.com>
2025-05-04 12:25:31 +0300

    Return To Auth

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth';
+import cors from 'cors';
+// Load environment variables
+dotenv.config();
+
+// use cors
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Welcome route
+app.get('/', (_req, res) => {
+  res.send('Hello,  basel!');
+});
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
