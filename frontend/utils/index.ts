fc5e5e8a0b1c6c21f9820119f5ad48d20d94ce9c
Kareem Ahmed <kareemsamir166@gmail.com>
2025-05-10 13:53:46 +0300

    FIX

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,9 +1,8 @@
 export const getUrl = () => {
   if (typeof window !== 'undefined') {
-    // We're in the browser — use the public env variable
-    return process.env.NEXT_PUBLIC_BACKEND_URL;
+    // client
+    return process.env.NEXT_PUBLIC_BACKEND_URL!;
   }
-
-  // For SSR/server-side (optional)
-  return process.env.BACKEND_URL;
+  // server
+  return process.env.BACKEND_URL || process.env.NEXT_PUBLIC_BACKEND_URL!;
 };
